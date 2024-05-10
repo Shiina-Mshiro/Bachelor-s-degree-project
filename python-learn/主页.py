714e70cea1707455ddbc57c0cab678546fbb4555
秋叶 <14397351+Schwi-Dola@user.noreply.gitee.com>
2024-05-10 18:07:23 +0800

    degree project

---
+++ python-learn/主页.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.title("  ")
+st.title("  ")
+st.title("  ")
+st.title("  ")
+st.title("  ")
+st.title("  ")
+st.title("饱和蒸汽p-t关系模拟仿真实验")
+
+
+# streamlit run 主页.py   CTRL c
+
+
+
+
